5fcc60167230ab0e3635095b9046b573850722bf
Neil Toledo <ntoledo@berkeley.edu>
2020-06-19 01:09:40 -0700

    intial commit

---
+++ main.js
@@ -0,0 +1,64 @@
+window.onload = function () {
+    "use strict";
+    var h = document.getElementsByTagName('h1')[0];
+    var report = 0;
+    
+    var soundAllowed = function (stream) {
+        //Audio stops listening in FF without // window.persistAudioStream = stream;
+        //https://bugzilla.mozilla.org/show_bug.cgi?id=965483
+        //https://support.mozilla.org/en-US/questions/984179
+        window.persistAuidoStream = stream;
+        h.innerHTML = "Thanks";
+        h.setAttribute('style', 'opacity: 0;');
+        var audioContent = new AudioContext();
+        var audioStream = audioContent.createMediaStreamSource( stream );
+        var analyser = audioContent.createAnalyser();
+        audioStream.connect(analyser);
+        analyser.fftSize = 1024;
+
+        var frequencyArray = new Uint8Array(analyser.frequencyBinCount);
+        frequencyArray = frequencyArray.slice(0, 256)
+        console.log(frequencyArray);
+
+        var loudness = 0;
+        var max = 0;
+        var avatar = document.getElementById('avatar');
+        var doDraw = function () {
+            requestAnimationFrame(doDraw);
+            analyser.getByteFrequencyData(frequencyArray);
+            var maxVal = Math.max.apply(Math, frequencyArray.slice(16, 256));
+            max = (max > maxVal) ? max : maxVal;
+            loudness = frequencyArray.reduce(function(a, b) {
+                if (b < 40) {
+                    return a;
+                } else {
+                    return a + b;
+                }
+            }, 0) / frequencyArray.length;
+
+            if (loudness > 80) {
+                avatar.src = avatar.getAttribute("yelling");
+            } else if (loudness > 60) {
+                avatar.src = avatar.getAttribute("normal");
+            } else {
+                avatar.src = avatar.getAttribute("idle");
+            }
+
+        }
+        // setInterval(doDraw, 10);
+        doDraw();
+    }
+
+    var soundNotAllowed = function (error) {
+        h.innerHTML = "You must allow your microphone.";
+        console.log(error);
+    }
+
+    /*window.navigator = window.navigator || {};
+    /*navigator.getUserMedia =  navigator.getUserMedia       ||
+                              navigator.webkitGetUserMedia ||
+                              navigator.mozGetUserMedia    ||
+                              null;*/
+    navigator.getUserMedia({audio:true}, soundAllowed, soundNotAllowed);
+
+};
